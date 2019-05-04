8419883b6aac3b710a687ab62e4bb14b2e613036
jacobhillman1 <jacobhillman1@gmail.com>
2019-05-03 20:28:38 -0400

    change address based on tables

--- load_balancer.c
+++ load_balancer.c
@@ -47,6 +47,21 @@ static unsigned int ip_hdrlen(const struct sk_buff *skb) {
     return ip_hdr(skb)->ihl * 4;
 }
 
+static unsigned int get_ip_from_tuple(char *tuple) {
+    int a,b,c,d,e;
+    char arr[4];
+    sscanf(tuple,"%d.%d.%d.%d:%d",&a,&b,&c,&d,&e);
+    arr[0] = a; arr[1] = b; arr[2] = c; arr[3] = d;
+    return *(unsigned int*)arr;
+}
+
+static __u16 get_port_from_tuple(char *tuple) {
+    int a,b,c,d;
+    __u16 e;
+    sscanf(tuple,"%d.%d.%d.%d:%hu",&a,&b,&c,&d,&e);
+    return e;
+}
+
 // print params (used for testing)
 void print_params(void) {
     int i;
@@ -63,6 +78,13 @@ void print_permutation(void) {
         for (j = 0; j < LOOKUP_SIZE; j++){
             pr_info("[%i][%i] = %i\n", i, j, permutation[i][j]);
         }
+    }
+}
+
+void print_lookup(void) {
+    int i;
+    for (i = 0; i < LOOKUP_SIZE; i++) {
+        pr_info("[%i] = %s", i, lookup_table[i]);
     }
 }
 
@@ -107,18 +129,21 @@ static void pop_permutation(void) {
  * in the permutation table
  */
 static int pop_lookup(void) {
-    int i, n, c = 0;
+    int i, n, c;
+    int *next;
+    n = 0;
+
     // keep track of the index for each backend in the permutation table
-    int *next = (int*)kmalloc(sizeof(int) * num, GFP_ATOMIC);
+    next = (int*)kmalloc(sizeof(int) * num, GFP_ATOMIC);
     for (i = 0; i < num; i++) {
         next[i] = 0;
     }
 
-    while (true) {
+    while (1) {
         for (i = 0; i < num; i++) {
             // find next open preference index for backend i
             c = permutation[i][next[i]];
-            while (lookup_table[c] >= 0) {
+            while (lookup_table[c] != NULL) {
                 next[i] += 1;
                 c = permutation[i][next[i]];
             }
@@ -127,12 +152,14 @@ static int pop_lookup(void) {
             next[i] += 1;
             n +=1;
             if (n == LOOKUP_SIZE) {
+                kfree(next);
                 return 1;
             }
         }
     }
 
     // shouldn't get here
+    kfree(next);
     return 0;
 } 
 
@@ -144,12 +171,15 @@ unsigned int fn_hook_incoming(void *priv,
     struct udphdr *udp_header; // UDP header struct
     char buffer[100]; // buffer for 5-tuple
     int tuple_hash; // hash for 5-tuple
+    char *backend; // where to forward the packet
 
     if (!skb) {
         return NF_ACCEPT;
     }
 
     ip_header = (struct iphdr *)skb_network_header(skb);
+
+    // TODO: some sort of check to decide if we want to forward the packet
 
     if (ip_header->protocol == IPPROTO_UDP) {
         udp_header = (struct udphdr *)(skb_transport_header(skb) + 
@@ -166,7 +196,23 @@ unsigned int fn_hook_incoming(void *priv,
 
             // get hash for 5-tuple
             tuple_hash = hash(buffer);
-            pr_info("hash value = %i", tuple_hash);
+
+            if (connection_table[tuple_hash] == NULL) {
+                // route has not yet been given to this connection
+                // choose a backend to send the packet to and add to connection table
+                backend = lookup_table[tuple_hash];
+                connection_table[tuple_hash] = backend;
+            } else {
+                backend = connection_table[tuple_hash];
+            }
+
+            // change values of the packet
+            ip_header->daddr = get_ip_from_tuple(backend);
+            udp_header->dest = get_port_from_tuple(backend);
+
+            pr_info("%pI4:%hu", &ip_header->daddr, udp_header->dest);
+
+            return NF_ACCEPT;
         }
     }
     // TEST changing the destination addr (hopefully packet will never arrive)
@@ -183,21 +229,24 @@ static int loadbalancer_init(void)
     // TODO: does this need to be atomic? bc not interrupt handler
     connection_table = (char**)kmalloc(sizeof(char*) * LOOKUP_SIZE, GFP_ATOMIC);
     for (i = 0; i < LOOKUP_SIZE; i++) {
-        connection_table[i] = (char*)kmalloc(sizeof(char) * VALUE_SIZE, GFP_ATOMIC);
+        connection_table[i] = NULL;
     }
 
     lookup_table = (char**)kmalloc(sizeof(char*) * LOOKUP_SIZE, GFP_ATOMIC);
-    // for (i = 0; i < LOOKUP_SIZE; i++) {
-    //     lookup_table[i] = (char*)kmalloc(sizeof(char) * VALUE_SIZE, GFP_ATOMIC);
-    // }
+    for (i = 0; i < LOOKUP_SIZE; i++) {
+        lookup_table[i] = NULL;
+    }
 
     permutation = (int**)kmalloc(sizeof(int*) * num, GFP_ATOMIC);
     for (i = 0; i < num; i++) {
         permutation[i] = (int*)kmalloc(sizeof(int) * LOOKUP_SIZE, GFP_ATOMIC);
     }
 
-    pop_permutation();
-    pop_lookup();
+    if (num != 0) { // these funcs will break if no params passed in
+        pop_permutation();
+        pop_lookup();
+        // print_lookup();
+    }
 
     // register pre-routing hook
     nfho_in.hook = fn_hook_incoming;
